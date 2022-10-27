80c0b58050e44a416dba416ab77561e2c8b6b59b
William Skou Heidemann <wihe@itu.dk>
2022-10-27 21:53:56 +0200

    test passes

---
+++ GitInsight.Tests/UnitTest1.cs
@@ -0,0 +1,61 @@
+using System.Reflection;
+using LibGit2Sharp;
+
+namespace GitInsight.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Running_Program_Outputs_Expected_Log()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+        var expectedOutput = File.OpenText("../../../ExpectedOutput.txt").ReadToEnd().TrimEnd();
+
+        // Act
+        var program = Assembly.Load(nameof(GitInsight));
+        
+        program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be(expectedOutput);
+    }
+    
+    
+    // [Fact]
+    // public void Running_Program_With_Frequency_Flag_Outputs_Frequency_Log()
+    // {
+    //     // Arrange
+    //     using var writer = new StringWriter();
+    //     Console.SetOut(writer);
+    //     var expectedOutput = File.OpenText("../../../ExpectedCommitFrequencyLog.txt").ReadToEnd().TrimEnd();
+    //
+    //     // Act
+    //     var program = Assembly.Load(nameof(GitInsight));
+    //     program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+    //
+    //     // Assert
+    //     var output = writer.GetStringBuilder().ToString().TrimEnd();
+    //     output.Should().Be(expectedOutput);
+    // }
+    //
+    // [Fact]
+    // public void Running_Program_With_Author_Flag_Outputs_Author_Log()
+    // {
+    //     // Arrange
+    //     using var writer = new StringWriter();
+    //     Console.SetOut(writer);
+    //     var expectedOutput = File.OpenText("../../../ExpectedCommitFrequencyLog.txt").ReadToEnd().TrimEnd();
+    //
+    //     // Act
+    //     var program = Assembly.Load(nameof(GitInsight));
+    //     
+    //     program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+    //
+    //     // Assert
+    //     var output = writer.GetStringBuilder().ToString().TrimEnd();
+    //     output.Should().Be(expectedOutput);
+    // }
+}
--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using LibGit2Sharp;
+
+Repository repository = new("C:\\Users\\Willi\\Desktop\\BDSA\\assignment-05");
+var commitlog = repository.Commits;
+
+Console.WriteLine("-------COMMIT FREQUENCY-------");
+foreach (var group in commitlog
+             .GroupBy(commit => commit.Author.When.Date)
+             .OrderBy(commits => commits.Key.Date))
+{
+    Console.WriteLine(group.Count() + " " + group.Key.ToShortDateString());
+}
+
+Console.WriteLine();
+Console.WriteLine("-------AUTHOR COMMITS-------");
+foreach (var authorcommits in commitlog
+             .GroupBy(commit => commit.Author.Email)
+             .OrderBy(commits => commits.Key))
+{
+    Console.WriteLine(authorcommits.Key);
+    foreach (var group in authorcommits
+                 .GroupBy(commit => commit.Author.When.Date)
+                 .OrderBy(commits => commits.Key.Date))
+    {
+        Console.WriteLine(group.Count() + " " + group.Key.ToShortDateString());
+    }
+    Console.WriteLine();
+}
