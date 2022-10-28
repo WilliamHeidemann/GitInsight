cb25a51cc2e2a977a5b3460015bf957e2c9d8028
A-Guldborg <aguh@itu.dk>
2022-10-28 12:38:08 +0200

    norestore on build in github actions

--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibGit2Sharp;
+
+using var test = new Repository(Repository.Clone(@"https://github.com/WilliamHeidemann/GitInsight", "./test"));
+
+foreach (var commit in test.Commits) {
+    Console.WriteLine(commit.Author.Name);
+}
+
+var sig = new Signature("Andreas", "aguh@itu.dk", DateTimeOffset.Now);
+
+test.Commit("Test message", sig, sig);
+test.Commit("Second commit", sig, sig);
+
+foreach (var commit in test.Commits) {
+    Console.WriteLine(commit.Author.Name);
+}
+
+
