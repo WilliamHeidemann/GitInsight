fd5fafbf1e9fda4962c34520bfa1b3221dbe1096
rakul <rakt@itu.dk>
2022-10-28 14:33:05 +0200

    Fixed commit counter + printing so pretty now

--- GitInsight/GitCommitTracker.cs
+++ GitInsight/GitCommitTracker.cs
@@ -10,28 +10,36 @@ public class GitCommitTracker
         _repository = Repository.IsValid(path) ? new Repository(path) : throw new ArgumentException("Repository was not found.");
     }
 
-    public IEnumerable<string> GetCommitFrequency() 
-        => GetCommitFrequencyByAuthor(_repository.Commits);
+    // public IEnumerable<string> GetCommitFrequency() 
+    //     => GetCommitFrequencyByAuthor(_repository.Commits);
 
-    public IEnumerable<string> GetCommitAuthor() {
+    public IEnumerable<string> GetCommitAuthor() 
+    {
+        
         foreach (var authorcommits in _repository.Commits
-             .GroupBy(commit => commit.Author.Email)
-             .OrderBy(commits => commits.Key))
+             .ToList()
+             .GroupBy(commit => commit.Committer.Name)
+             .Distinct())
         {        
-            yield return authorcommits.Key;
-            foreach (var commit in GetCommitFrequencyByAuthor(authorcommits)) {
-                yield return commit;
+
+            yield return authorcommits.FirstOrDefault()!.Committer.Name;
+            foreach (var commit in authorcommits
+                    .GroupBy(r => r.Committer.When.Date)
+                    .Select(g => new {count = g.Count(), date = g.First().Committer.When.Date.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture)})) {
+                var space = commit.count > 9 ? "    " : "     ";
+                yield return space + commit.count+ " " + commit.date;
             }
             yield return "";
         }
     }
 
-    private IEnumerable<string> GetCommitFrequencyByAuthor(IEnumerable<Commit> authorCommits) {
+    public IEnumerable<string> GetCommitFrequency() {
          foreach (var commitsByDay in _repository.Commits
              .GroupBy(commit => commit.Author.When.Date)
              .OrderBy(commits => commits.Key.Date))
         {
-            yield return commitsByDay.Count() + " " + commitsByDay.Key.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
+            var space = commitsByDay.Count() > 9 ? "    " : "     ";
+            yield return space + commitsByDay.Count() + " " + commitsByDay.Key.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
         }
     }
 }
