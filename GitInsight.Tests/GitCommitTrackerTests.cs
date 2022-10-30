4b37784824eb5ed9b8a0aae415b2e54cf8c28696
William Skou Heidemann <wihe@itu.dk>
2022-10-30 21:58:56 +0100

    Removed code duplication in GitCommitTracker. Changed from foreach to select statement in frequency method. Introduced string interpolation. Removed commented out code.

--- GitInsight.Tests/GitCommitTrackerTests.cs
+++ GitInsight.Tests/GitCommitTrackerTests.cs
@@ -5,7 +5,7 @@ namespace GitInsight.Tests;
 
 public class GitCommitTrackerTests
 {
-    public readonly string _testPath;
+    private readonly string _testPath;
 
     public GitCommitTrackerTests() 
     {
@@ -73,61 +73,6 @@ public class GitCommitTrackerTests
     {
         Action invalidRepo = () => new GitCommitTracker("invalid");
 
-        invalidRepo.Should().Throw<ArgumentException>().WithMessage("Repository was not found.");
+        invalidRepo.Should().Throw<ArgumentException>().WithMessage($"Repository was not found at invalid.");
     }
-
-    /*
-    [Fact]
-    public void Running_Program_Outputs_Expected_Log()
-    {
-        // Arrange
-        using var writer = new StringWriter();
-        Console.SetOut(writer);
-        var expectedOutput = File.OpenText("../../../ExpectedOutput.txt").ReadToEnd().TrimEnd();
-
-        // Act
-        var program = Assembly.Load(nameof(GitCommitTracker));
-        
-        program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
-
-        // Assert
-        var output = writer.GetStringBuilder().ToString().TrimEnd();
-        output.Should().Be(expectedOutput);
-    }
-    
-    */
-    // [Fact]
-    // public void Running_Program_With_Frequency_Flag_Outputs_Frequency_Log()
-    // {
-    //     // Arrange
-    //     using var writer = new StringWriter();
-    //     Console.SetOut(writer);
-    //     var expectedOutput = File.OpenText("../../../ExpectedCommitFrequencyLog.txt").ReadToEnd().TrimEnd();
-    //
-    //     // Act
-    //     var program = Assembly.Load(nameof(GitInsight));
-    //     program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
-    //
-    //     // Assert
-    //     var output = writer.GetStringBuilder().ToString().TrimEnd();
-    //     output.Should().Be(expectedOutput);
-    // }
-    //
-    // [Fact]
-    // public void Running_Program_With_Author_Flag_Outputs_Author_Log()
-    // {
-    //     // Arrange
-    //     using var writer = new StringWriter();
-    //     Console.SetOut(writer);
-    //     var expectedOutput = File.OpenText("../../../ExpectedCommitFrequencyLog.txt").ReadToEnd().TrimEnd();
-    //
-    //     // Act
-    //     var program = Assembly.Load(nameof(GitInsight));
-    //     
-    //     program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
-    //
-    //     // Assert
-    //     var output = writer.GetStringBuilder().ToString().TrimEnd();
-    //     output.Should().Be(expectedOutput);
-    // }
 }
