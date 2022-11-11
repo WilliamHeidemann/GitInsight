a3b45bb0cb2f2aee3c228558c1712123eae1f0bf
A-Guldborg <aguh@itu.dk>
2022-11-11 13:17:50 +0100

    Test webendpoint

---
+++ GitInsight.WebEndPoint.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace GitInsight.WebEndPoint.Tests;
+
+public class UnitTest1
+{
+    private readonly IntegrationTestFactory _factory;
+
+    public UnitTest1(IntegrationsTestFactory factory) {
+        _factory = factory;
+    }
+    [Fact]
+    public void Test1()
+    {
+        
+    }
+}
