31b1496f5ac7a3a353eb1a5c484c74dbd7249519
Oliver Friis <94053214+OliFryser@users.noreply.github.com>
2022-11-14 22:29:10 +0100

    Primary key fix (#77)
    * Commits have new composite primary key
    
    * Test fix

--- GitInsight.Infrastructure/Migrations/20221114210150_NewKeyForCommits.Designer.cs
+++ GitInsight.Infrastructure/Migrations/20221114210150_NewKeyForCommits.Designer.cs
@@ -2,6 +2,7 @@
 using GitInsight.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GitInsight.Infrastructure.Migrations
 {
     [DbContext(typeof(PersistentStorageContext))]
-    partial class PersistentStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20221114210150_NewKeyForCommits")]
+    partial class NewKeyForCommits
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
