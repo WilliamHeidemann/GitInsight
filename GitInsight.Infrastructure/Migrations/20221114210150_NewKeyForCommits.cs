33b8922d414c75ae3ad39d185bdb5b764cb3f818
oljh <oljh@itu.dk>
2022-11-14 22:27:04 +0100

    Test fix

---
+++ GitInsight.Infrastructure/Migrations/20221114210150_NewKeyForCommits.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GitInsight.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewKeyForCommits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Commits",
+                table: "Commits");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Commits",
+                table: "Commits",
+                columns: new[] { "SHA", "RepoId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Commits",
+                table: "Commits");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Commits",
+                table: "Commits",
+                column: "SHA");
+        }
+    }
+}
