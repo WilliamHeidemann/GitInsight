51a013fc3d938b49119ab29416f327a64a5439cc
Mads <mroa@itu.dk>
2022-11-19 14:34:30 +0100

    Repo input implemented

--- BlazorApp/Data/StartPageModel.cs
+++ BlazorApp/Data/StartPageModel.cs
@@ -3,6 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 public class StartPageModel
 {
+    public bool IsValidRepo { get; set; }
     [Required]
+    [MinLength(1, ErrorMessage = "RepoPath must not be empty")]
     public string? RepoPath { get; set; }
 }
